2f8dc77a3f6cdb2de04a2df1f47829de261029f4
Nikol <Nikol@DESKTOP-NRSH0JP>
2016-11-30 08:47:53 +0100

    Add project files.

---
+++ EmailService/EmailService.svc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmailService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmailService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmailService.svc or EmailService.svc.cs at the Solution Explorer and start debugging.
+
+    public class EmailService : IEmailService
+    {
+        private static string SMTPSERVER = "smtp.gmail.com";
+        private static int PORTNO = 587;
+
+        public string SendEmail(string gmailUserName, string gmailUserPassword,
+           string[] emailToAddress, string[] ccemailTo, string subject, string body, bool isBodyHtml)
+        {
+            if (gmailUserName == null || gmailUserName.Trim().Length == 0)
+            {
+                return "User Name Empty";
+            }
+            if (gmailUserPassword == null || gmailUserPassword.Trim().Length == 0)
+            {
+                return "Email Password Empty";
+            }
+            if (emailToAddress == null || emailToAddress.Length == 0)
+            {
+                return "Email To Address Empty";
+            }
+
+            List<string> tempFiles = new List<string>();
+
+            SmtpClient smtpClient = new SmtpClient(SMTPSERVER, PORTNO);
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(gmailUserName, gmailUserPassword);
+            using (MailMessage message = new MailMessage())
+            {
+                message.From = new MailAddress(gmailUserName);
+                message.Subject = subject == null ? "" : subject;
+                message.Body = body == null ? "" : body;
+                message.IsBodyHtml = isBodyHtml;
+
+                foreach (string email in emailToAddress)
+                {
+                    message.To.Add(email);
+                }
+                if (ccemailTo != null && ccemailTo.Length > 0)
+                {
+                    foreach (string emailCc in ccemailTo)
+                    {
+                        message.CC.Add(emailCc);
+                    }
+                }
+                try
+                {
+                    smtpClient.Send(message);
+                    return "Email Send SuccessFully";
+                }
+                catch
+                {
+                    return "Email Send failed";
+                }
+            }
+        }
+    }
+}
---
+++ EmailService/IEmailService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmailService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IEmailService
+    {
+        [OperationContract]
+        string SendEmail(string gmailUserAddress, string gmailUserPassword,
+        string[] emailTo, string[] ccTo, string subject, string body, bool isBodyHtml);
+    }
+}
+
